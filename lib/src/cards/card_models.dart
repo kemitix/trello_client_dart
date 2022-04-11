f23c527101b14ebfca9a6f79b87abee02b74d27d
Paul Campbell <pcampbell@kemitix.net>
2022-04-11 07:42:13 +0100

    Card: parse datetime fields

--- lib/src/cards/card_models.dart
+++ lib/src/cards/card_models.dart
@@ -12,10 +12,11 @@ class Card {
   CardBadges get badges => _getValue(CardFields.badges);
   //List<?> get checkItemStates => _getValue(CardFields.checkItemStates);
   bool get closed => _getValue(CardFields.closed);
-  DateTime get dateLastActivity => _getValue(CardFields.dateLastActivity);
+  DateTime? get dateLastActivity =>
+      _dateTime(_getValue(CardFields.dateLastActivity));
   String get desc => _getValue(CardFields.desc);
   //? get descData => _getValue(CardFields.descData);
-  DateTime get due => _getValue(CardFields.due);
+  DateTime? get due => _dateTime(_getValue(CardFields.due));
   bool get dueComplete => _getValue(CardFields.dueComplete);
   String get idAttachmentCover => _getValue(CardFields.idAttachmentCover);
   String get idBoard => _getValue(CardFields.idBoard);
@@ -44,6 +45,11 @@ class Card {
     throw AssertionError(
         'List: Attempt to access field not retrieved: ${field.name}');
   }
+}
+
+DateTime? _dateTime(String? value) {
+  if (value == null) return null;
+  return DateTime.parse(value);
 }
 
 // Field vale may come from Trello as 'latitude,longitude'
