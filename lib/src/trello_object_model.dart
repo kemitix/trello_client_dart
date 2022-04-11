51b01e1f1b9abe89ba1975435b2c104f8d4a32a2
Paul Campbell <pcampbell@kemitix.net>
2022-04-11 18:53:49 +0100

    Added TrelloObject

---
+++ lib/src/trello_object_model.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+
+abstract class TrelloObject<T extends Enum> {
+  final dynamic _source;
+  final List<T> _fields;
+  late final bool _all; // were all fields selected'
+
+  TrelloObject(this._source, this._fields, {all: bool}) {
+    _all = all;
+  }
+
+  dynamic get raw => _source;
+
+  @protected
+  V getValue<V>(T field) {
+    if (_all || _fields.contains(field)) {
+      return _source[field.name];
+    }
+    throw AssertionError(
+        'Attempt to access field not retrieved: ${field.name}');
+  }
+}
