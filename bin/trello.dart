65912272671c002a95e9afc601bfc8ba7b236f1f
Paul Campbell <pcampbell@kemitix.net>
2022-04-16 09:16:44 +0100

    Strip out bulk of original cli implementation

--- bin/trello.dart
+++ bin/trello.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:cli_dialog/cli_dialog.dart';
 import 'package:trello_sdk/trello_sdk.dart';
 
 typedef Error = String;
@@ -12,116 +11,8 @@ Future<void> main(List<String> arguments) async =>
           onSuccess: (client) => runApp(client),
         );
 
-const String choiceQuit = '<< Quit';
-const String choiceBack = '< Back';
-const String separator = '--------------------------';
-
 Future<void> runApp(TrelloClient client) async {
-  List<String> menu = [choiceQuit, 'Open Board'];
-  while (true) {
-    print(separator);
-    String choice = listQuestion(menu, message: "Select: ");
-    switch (choice) {
-      case choiceQuit:
-        client.close();
-        exit(0);
-      case 'Open Board':
-        await selectBoard(client);
-    }
-  }
-}
-
-String listQuestion(List<String> menu, {required String message}) {
-  return CLI_Dialog(listQuestions: [
-    [
-      {
-        'question': message,
-        'options': menu,
-      },
-      'question'
-    ]
-  ]).ask()['question'];
-}
-
-Future<void> selectBoard(TrelloClient client) async {
-  List<Board> boards = await client
-      .member(client.memberId)
-      .getBoards(fields: [BoardFields.id, BoardFields.name]);
-  List<String> menu = boards.map((board) => board.name).toList();
-  menu.insert(0, choiceBack);
-  while (true) {
-    print(separator);
-    String choice = listQuestion(menu, message: "Select Board: ");
-    switch (choice) {
-      case choiceBack:
-        return;
-      default:
-        BoardId boardId = boards
-            .where((board) => board.name == choice)
-            .map((board) => board.id)
-            .first;
-        print({boardId});
-        await selectList(boardId, choice, client);
-    }
-  }
-}
-
-Future<void> selectList(
-    BoardId boardId, String boardName, TrelloClient client) async {
-  List<TrelloList> lists = await client
-      .board(boardId)
-      .getLists(fields: [ListFields.id, ListFields.name]);
-  List<String> menu = lists.map((list) => list.name).toList();
-  menu.insert(0, choiceBack);
-  while (true) {
-    print(separator);
-    String choice = listQuestion(menu, message: "Select List: ");
-    switch (choice) {
-      case choiceBack:
-        return;
-      default:
-        ListId listId = lists
-            .where((list) => list.name == choice)
-            .map((list) => list.id)
-            .first;
-        print({listId});
-        await selectCard(listId, choice, boardName, client);
-    }
-  }
-}
-
-Future<void> selectCard(ListId listId, String listName, String boardName,
-    TrelloClient client) async {
-  List<Card> cards = await client
-      .list(listId)
-      .getCards(fields: [CardFields.id, CardFields.name]);
-  List<String> menu = cards.map((card) => card.name).toList();
-  menu.insert(0, choiceBack);
-  while (true) {
-    print(separator);
-    String choice = listQuestion(menu, message: "Select Card: ");
-    switch (choice) {
-      case choiceBack:
-        return;
-      default:
-        CardId cardId = cards
-            .where((card) => card.name == choice)
-            .map((card) => card.id)
-            .first;
-        print({cardId});
-        await showCard(cardId, choice, listName, boardName, client);
-    }
-  }
-}
-
-Future<void> showCard(CardId cardId, String cardName, String listName,
-    String boardName, TrelloClient client) async {
-  Card? card = await client.card(cardId).get();
-  if (card != null) {
-    print('Card: ${card.id} - ${card.name}');
-  } else {
-    print('Card not found');
-  }
+  //TODO
 }
 
 Future<void> reportErrors(ErrorList errors) async => errors.forEach(print);
@@ -133,7 +24,7 @@ Either<ErrorList, TrelloAuthentication> authentication() {
   var e = <String>[];
   void testEnvironment(String key) {
     if (!Platform.environment.keys.contains(key)) {
-      e.add('Environment no set ${key}');
+      e.add('Environment no set $key');
     }
   }
 
