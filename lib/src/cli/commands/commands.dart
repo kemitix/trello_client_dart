1b4d10dfa8e5ff93d14de19e494ca07f2ac34f6b
Paul Campbell <pcampbell@kemitix.net>
2022-04-16 13:02:37 +0100

    refactoring

--- lib/src/cli/commands/commands.dart
+++ lib/src/cli/commands/commands.dart
@@ -4,7 +4,21 @@ import 'package:trello_sdk/trello_sdk.dart';
 export 'member_command.dart';
 
 abstract class TrelloCommand extends Command {
-  final TrelloClient client;
+  final String _name;
+  final String _description;
+  final TrelloClient _client;
 
-  TrelloCommand(this.client);
+  TrelloCommand(
+    this._name,
+    this._description,
+    this._client,
+  );
+
+  @override
+  String get name => _name;
+  @override
+  String get description => _description;
+  TrelloClient get client => _client;
+
+  List<String> get parameters => argResults!.rest;
 }
--- lib/src/cli/commands/member_command.dart
+++ lib/src/cli/commands/member_command.dart
@@ -5,31 +5,26 @@ import 'package:args/command_runner.dart';
 import '../../../trello_sdk.dart';
 import '../cli.dart';
 
-class MemberCommand extends TrelloCommand {
+class MemberCommand extends Command {
   @override
   final String name = 'member';
   @override
   final String description = 'Trello Members (users)';
 
-  MemberCommand(TrelloClient client) : super(client) {
-    addSubcommand(GetMemberCommand(client));
+  MemberCommand(TrelloClient client) {
+    [GetMemberCommand(client)].forEach(addSubcommand);
   }
 }
 
 class GetMemberCommand extends TrelloCommand {
-  @override
-  final String name = 'get';
-  @override
-  final String description = 'Get a member';
+  GetMemberCommand(TrelloClient client) : super('get', 'Get a member', client);
 
-  GetMemberCommand(TrelloClient client) : super(client);
+  final String errorIdMissing = 'Member Id was not given';
 
   @override
   FutureOr<void> run() async {
-    if (argResults!.rest.isEmpty) {
-      throw UsageException('Member Id not given', usage);
-    }
-    MemberId id = MemberId(argResults!.rest.first);
+    if (parameters.isEmpty) throw UsageException(errorIdMissing, usage);
+    MemberId id = MemberId(parameters.first);
     Member member = await client.member(id).get();
     print(member.url);
   }
