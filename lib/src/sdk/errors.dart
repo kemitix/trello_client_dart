552c000916cf666533096e884776d8a838c73453
Paul Campbell <pcampbell@kemitix.net>
2022-04-22 08:32:02 +0100

    nextParameter returns an Either
    Add demo

--- lib/src/sdk/errors.dart
+++ lib/src/sdk/errors.dart
@@ -1,1 +1,22 @@
-class Failure {}
+abstract class Failure {
+  Failure({required String message}) {
+    _message = message;
+  }
+
+  late final String _message;
+
+  String get message => _message;
+}
+
+class HttpClientFailure extends Failure {
+  HttpClientFailure({required String message}) : super(message: message);
+}
+
+class ResourceNotFoundFailure extends Failure {
+  ResourceNotFoundFailure({required String resource})
+      : super(message: 'Resource not found: $resource}');
+}
+
+class UsageFailure extends Failure {
+  UsageFailure({required String usage}) : super(message: usage);
+}
