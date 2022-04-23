6070a3216a9d07aceb884de173c550e7e710a0b7
Paul Campbell <pcampbell@kemitix.net>
2022-04-23 13:52:33 +0100

    Failure can now gather context

--- lib/src/sdk/errors.dart
+++ lib/src/sdk/errors.dart
@@ -1,32 +1,52 @@
 abstract class Failure {
-  Failure({required String message}) {
+  Failure({
+    required String message,
+    Map<String, String>? context,
+  }) {
     _message = message;
+    _context = context ?? {};
   }
 
   late final String _message;
+  late Map<String, String> _context;
 
   String get message => _message;
 
   @override
   String toString() {
-    return 'Failure: $_message';
+    return 'Failure: $_message - $_context';
+  }
+
+  Failure withContext(Map<String, String> additionalContext) {
+    _context.addAll(additionalContext);
+    return this;
   }
 }
 
 class HttpClientFailure extends Failure {
-  HttpClientFailure({required String message}) : super(message: message);
+  HttpClientFailure({
+    required String message,
+    Map<String, String>? context,
+  }) : super(message: message, context: context);
 }
 
 class ResourceNotFoundFailure extends Failure {
-  ResourceNotFoundFailure({required String resource})
-      : super(message: 'Resource not found: $resource}');
+  ResourceNotFoundFailure({
+    required String resource,
+    Map<String, String>? context,
+  }) : super(message: 'Resource not found: $resource}', context: context);
 }
 
 class UsageFailure extends Failure {
-  UsageFailure({required String usage}) : super(message: usage);
+  UsageFailure({
+    required String usage,
+    Map<String, String>? context,
+  }) : super(message: usage, context: context);
 }
 
 class NotImplementedFailure extends Failure {
-  NotImplementedFailure({required String function})
-      : super(message: 'Function not implmented: $function');
+  NotImplementedFailure({
+    required String function,
+    Map<String, String>? context,
+  }) : super(message: 'Function not implemented: $function', context: context);
 }
