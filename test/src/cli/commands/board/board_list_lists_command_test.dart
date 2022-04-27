c36473bb7153e189e7f3d19cc47526bd0584c4c5
Paul Campbell <pcampbell@kemitix.net>
2022-04-27 18:29:33 +0100

    clean up

--- test/src/cli/commands/board/board_list_lists_command_test.dart
+++ test/src/cli/commands/board/board_list_lists_command_test.dart
@@ -1,19 +1,11 @@
 import 'dart:convert';
 
-import 'package:args/command_runner.dart';
 import 'package:dio/dio.dart';
-import 'package:test/scaffolding.dart';
 import 'package:test/test.dart';
 import 'package:trello_sdk/trello_cli.dart';
 
 import '../../../mocks/dio_mock.dart';
 
-const dioHttpHeadersForResponseBody = {
-  Headers.contentTypeHeader: [Headers.jsonContentType],
-};
-
-/// https://gist.github.com/hjJunior/1ea524be7f0d09e5b1639e46560f44f0
-///
 void main() {
   var authentication =
       TrelloAuthentication.of(MemberId("_memberId"), "_key", "_token");
@@ -24,16 +16,18 @@ void main() {
     var response = ResponseBody.fromString(
       jsonEncode([]),
       200,
-      headers: dioHttpHeadersForResponseBody,
+      headers: {
+        Headers.contentTypeHeader: [Headers.jsonContentType],
+      },
     );
     var testClient = testTrelloClient(
         baseUrl: 'example.com',
         queryParameters: {'bar': 'baz'},
         authentication: authentication,
         responses: <ResponseBody>[response]);
-    CommandRunner commandRunner = runner(testClient.trelloClient);
+    var commandRunner = runner(testClient.trelloClient);
     //when
-    await commandRunner.run(['board', 'list-lists', 'my-board-id']);
+    await commandRunner.run(['board', 'list-lists', boardId]);
     //then
     expect(testClient.fetchHistory.length, 1);
     var requestOptions = testClient.fetchHistory[0].head;
