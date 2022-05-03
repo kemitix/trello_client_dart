8c0ee22ffcd5f88b72bcea57a7c19fad2a991107
Paul Campbell <pcampbell@kemitix.net>
2022-05-03 18:48:02 +0100

    add fakePrinter

--- test/src/cli/commands/board/board_list_lists_command_test.dart
+++ test/src/cli/commands/board/board_list_lists_command_test.dart
@@ -6,11 +6,17 @@ import '../../cli_commons.dart';
 void main() {
   var boardId = 'my-board-id';
   var args = 'board list-lists $boardId'.split(' ');
-  var fakeTrelloClient = createFakeTrelloClient([createResponse(body: [])]);
+  var fakeTrelloClient = createFakeTrelloClient([createResponse(body: [{
+      'id': 'my-list-id',
+      'name': 'my-list-name',
+      'pos': 1024,
+      'closed': false,
+      'subscribed': true,
+    }])]);
   var fetchHistory = fakeTrelloClient.fetchHistory;
-  var output = <String>[];
+  var fakePrinter = FakePrinter();
   setUpAll(() async => await app().run(EnvArgsEnvironment(validEnvironment,
-      args, (_) => fakeTrelloClient.trelloClient, (s) => output.add(s.toString()))));
+      args, (_) => fakeTrelloClient.trelloClient, fakePrinter.printer)));
   test('there was only one request', () => expect(fetchHistory.length, 1));
   test('request was a GET', () => expect(fetchHistory[0].head.method, 'GET'));
   test('request baseUrl',
@@ -25,5 +31,9 @@ void main() {
             'filter': 'all',
             'fields': 'id,name,pos,closed,subscribed',
           }));
-  test('output', () async => expect(output, ['Hello, World!']));
+  test('output', () async => expect(fakePrinter.output, [
+    'id         | name         |  pos | closed | subscribed',
+    '-----------|--------------|------|--------|-----------',
+    'my-list-id | my-list-name | 1024 | false  | true      ',
+  ]));
 }
