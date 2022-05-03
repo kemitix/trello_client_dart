01e3c44602b5d0a7ebf9798eec61fc187afdd407
Paul Campbell <pcampbell@kemitix.net>
2022-05-03 07:55:21 +0100

    simplify tests

--- test/src/cli/commands/board/board_list_lists_command_test.dart
+++ test/src/cli/commands/board/board_list_lists_command_test.dart
@@ -7,24 +7,18 @@ void main() {
   var boardId = 'my-board-id';
   var args = 'board list-lists $boardId'.split(' ');
   var fakeTrelloClient = createFakeTrelloClient([createResponse(body: [])]);
+  var fetchHistory = fakeTrelloClient.fetchHistory;
   setUpAll(() async => await app().run(EnvArgsEnvironment(validEnvironment,
       args, (TrelloAuthentication _) => fakeTrelloClient.trelloClient)));
-  test('there was only one request',
-      () => expect(fakeTrelloClient.fetchHistory.length, 1));
-  test('request was a GET',
-      () => expect(fakeTrelloClient.fetchHistory[0].head.method, 'GET'));
-  test(
-      'request baseUrl',
-      () =>
-          expect(fakeTrelloClient.fetchHistory[0].head.baseUrl, 'example.com'));
-  test(
-      'request path',
-      () => expect(fakeTrelloClient.fetchHistory[0].head.path,
-          '/1/boards/$boardId/lists'));
+  test('there was only one request', () => expect(fetchHistory.length, 1));
+  test('request was a GET', () => expect(fetchHistory[0].head.method, 'GET'));
+  test('request baseUrl',
+      () => expect(fetchHistory[0].head.baseUrl, 'example.com'));
+  test('request path',
+      () => expect(fetchHistory[0].head.path, '/1/boards/$boardId/lists'));
   test(
       'requests query parameters',
-      () async =>
-          expect(fakeTrelloClient.fetchHistory[0].head.queryParameters, {
+      () async => expect(fetchHistory[0].head.queryParameters, {
             'cards': 'all',
             'card_fields': 'all',
             'filter': 'all',
