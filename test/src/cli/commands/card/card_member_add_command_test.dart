3d6b0bd5fbb2b8fc85b548473ad0f2a1692f3c55
Paul Campbell <pcampbell@kemitix.net>
2022-05-07 08:24:39 +0100

    Replace card_member_add_command_test

--- test/src/cli/commands/card/card_member_add_command_test.dart
+++ test/src/cli/commands/card/card_member_add_command_test.dart
@@ -5,42 +5,93 @@ import '../../../mocks/dio_mock.dart';
 import '../../cli_commons.dart';
 
 void main() {
-  //given
-  var cardId = 'my-card-id';
-  var client = TestTrelloClient(responses: [
-    createResponse(body: {
-      'id': 'my-card-id',
-      'pos': 'my-pos',
-      'name': 'my-card-name',
-      'due': '2022-05-05T09:45',
-      'idMembers': ['my-member-id'],
-    })
-  ]);
-  var printer = FakePrinter();
-  var environment = EnvArgsEnvironment(
-    platformEnvironment: validEnvironment,
-    arguments: 'card get $cardId'.split(' '),
-    clientFactory: (_) => client.trelloClient,
-    printer: printer.printer,
-  );
+  group('add new member to card', () {
+    //given
+    var cardId = 'my-card-id';
+    var memberId = 'my-member-id';
+    var client = TestTrelloClient(responses: [
+      createResponse(body: {
+        'id': 'my-card-id',
+        'pos': 'my-pos',
+        'name': 'my-card-name',
+        'due': '2022-05-05T09:45',
+        'idMembers': [],
+      }),
+      createResponse(body: {})
+    ]);
+    var printer = FakePrinter();
+    var environment = EnvArgsEnvironment(
+      platformEnvironment: validEnvironment,
+      arguments: 'card add-member $cardId $memberId'.split(' '),
+      clientFactory: (_) => client.trelloClient,
+      printer: printer.printer,
+    );
 
-  //when
-  setUpAll(() => app().run(environment));
+    //when
+    setUpAll(() => app().run(environment));
 
-  //then
-  var history = client.fetchHistory;
-  test('there was one request', () => expect(history.length, 1));
-  test('request was GET', () => expect(history[0].head.method, 'GET'));
-  test('request path', () => expect(history[0].head.path, '/1/cards/$cardId'));
-  test('request query parameters',
-      () => expect(history[0].head.queryParameters, {'fields': 'all'}));
-  test(
-      'output',
-      () => expect(printer.output, [
-            'id        | my-card-id      ',
-            'name      | my-card-name    ',
-            'pos       | my-pos          ',
-            'due       | 2022-05-05T09:45',
-            'idMembers | [my-member-id]  '
-          ]));
+    //then
+    var history = client.fetchHistory;
+    test('there were two requests', () => expect(history.length, 2));
+
+    test('first request was GET', () => expect(history[0].head.method, 'GET'));
+    test('first request path', () =>
+        expect(history[0].head.path, '/1/cards/$cardId'));
+    test('first request query parameters',
+            () => expect(history[0].head.queryParameters, {'fields': 'idMembers'}));
+
+    test('second request was POST', () =>
+        expect(history[1].head.method, 'POST'));
+    test('second request path', () =>
+        expect(history[1].head.path, '/1/cards/$cardId/idMembers'));
+    test('second request query parameters',
+            () => expect(history[1].head.queryParameters, {
+              'value': memberId
+            }));
+
+    test(
+        'output',
+            () => expect(printer.output, [
+              'Added member'
+            ]));
+  });
+  group('do not add existing member to card', () {
+    //given
+    var cardId = 'my-card-id';
+    var memberId = 'my-member-id';
+    var client = TestTrelloClient(responses: [
+      createResponse(body: {
+        'id': 'my-card-id',
+        'pos': 'my-pos',
+        'name': 'my-card-name',
+        'due': '2022-05-05T09:45',
+        'idMembers': ['my-member-id'],
+      })
+    ]);
+    var printer = FakePrinter();
+    var environment = EnvArgsEnvironment(
+      platformEnvironment: validEnvironment,
+      arguments: 'card add-member $cardId $memberId'.split(' '),
+      clientFactory: (_) => client.trelloClient,
+      printer: printer.printer,
+    );
+
+    //when
+    setUpAll(() => app().run(environment));
+
+    //then
+    var history = client.fetchHistory;
+    test('there was one request', () => expect(history.length, 1));
+
+    test('request was GET', () => expect(history[0].head.method, 'GET'));
+    test('request path', () => expect(history[0].head.path, '/1/cards/$cardId'));
+    test('request query parameters',
+            () => expect(history[0].head.queryParameters, {'fields': 'idMembers'}));
+
+    test(
+        'output',
+            () => expect(printer.output, [
+              'ERROR: card add-member - Failure: Can\'t Add a Member to a Card as it is already applied'
+            ]));
+  });
 }
