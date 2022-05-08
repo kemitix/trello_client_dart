0c3318b7fb421b13c61155891d4056ae27179742
Paul Campbell <pcampbell@kemitix.net>
2022-05-08 11:04:50 +0100

    board_test: qualify board getLists as successful

--- test/src/sdk/boards/boards_test.dart
+++ test/src/sdk/boards/boards_test.dart
@@ -11,33 +11,35 @@ import '../sdk_commons.dart';
 
 void main() {
   group('getLists', () {
-    //given
-    var boardId = 'my-board-id';
-    var client = TestTrelloClient(responses: [
-      createResponse(body: [
-        {
-          'id': 'my-list-id',
-          'name': 'my-list-name',
-          'closed': true,
-          'idBoard': boardId,
-          'pos': 2000,
-          'subscribed': true,
-        }
-      ]),
-    ]);
-    var boardClient = client.trelloClient.board(BoardId(boardId));
-    late Either<Failure, TrelloList> firstList;
+    group('success', () {
+      //given
+      var boardId = 'my-board-id';
+      var client = TestTrelloClient(responses: [
+        createResponse(body: [
+          {
+            'id': 'my-list-id',
+            'name': 'my-list-name',
+            'closed': true,
+            'idBoard': boardId,
+            'pos': 2000,
+            'subscribed': true,
+          }
+        ]),
+      ]);
+      var boardClient = client.trelloClient.board(BoardId(boardId));
+      late Either<Failure, TrelloList> firstList;
 
-    //when
-    setUpAll(() async => firstList = await getFirstList(boardClient));
+      //when
+      setUpAll(() async => firstList = await getFirstList(boardClient));
 
-    //then
-    test('board id', () async => verify<TrelloList>(firstList, (r) => expect(r.id, ListId('my-list-id'))));
-    test('board name', () async => verify<TrelloList>(firstList, (r) => expect(r.name, 'my-list-name')));
-    test('board closed', () async => verify<TrelloList>(firstList, (r) => expect(r.closed, true)));
-    test('board idBoard', () async => verify<TrelloList>(firstList, (r) => expect(r.idBoard, BoardId(boardId))));
-    test('board pos', () async => verify<TrelloList>(firstList, (r) => expect(r.pos, 2000)));
-    test('board subscribed', () async => verify<TrelloList>(firstList, (r) => expect(r.subscribed, true)));
+      //then
+      test('board id', () async => verify<TrelloList>(firstList, (r) => expect(r.id, ListId('my-list-id'))));
+      test('board name', () async => verify<TrelloList>(firstList, (r) => expect(r.name, 'my-list-name')));
+      test('board closed', () async => verify<TrelloList>(firstList, (r) => expect(r.closed, true)));
+      test('board idBoard', () async => verify<TrelloList>(firstList, (r) => expect(r.idBoard, BoardId(boardId))));
+      test('board pos', () async => verify<TrelloList>(firstList, (r) => expect(r.pos, 2000)));
+      test('board subscribed', () async => verify<TrelloList>(firstList, (r) => expect(r.subscribed, true)));
+    });
   });
 }
 
