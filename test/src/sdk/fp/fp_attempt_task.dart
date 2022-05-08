163b0a749542cbf2baed7970a8a79bd749907c7d
Paul Campbell <pcampbell@kemitix.net>
2022-05-08 18:55:35 +0100

    update fp_attempt_task_test

--- test/src/sdk/fp/fp_attempt_task.dart
+++ test/src/sdk/fp/fp_attempt_task.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:test/test.dart';
 import 'package:trello_sdk/src/sdk/fp/fp.dart';
 
@@ -5,19 +7,19 @@ void main() {
   test('attemptTask catches thrown exception', () async {
     //given
     Future<int> action() async {
-      throw Exception('bar');
+      throw SocketException('bar');
     }
 
     late final TaskEither<String, int> result;
     //when
 
-    result = attemptTask(action).mapLeft((l) => "Foo: ${l.message}");
+    result = attemptTask(action).mapLeft((l) => "Foo: $l");
 
     //then
     try {
       Either<String, int> outcome = await result.run();
-      outcome.fold(
-          (l) => expect(l, 'Foo: bar'), (r) => fail('should have failed'));
+      outcome.fold((l) => expect(l, 'Foo: SocketException: bar'),
+          (r) => fail('should have failed'));
     } on Exception catch (ex) {
       fail('Not caught: $ex');
     }
