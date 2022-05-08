147c1b1f277701ce176f389f887986f7b8c34aaa
Paul Campbell <pcampbell@kemitix.net>
2022-05-08 20:30:03 +0100

    sdk_commons: extract apiTest

--- test/src/sdk/boards/boards_client_test.dart
+++ test/src/sdk/boards/boards_client_test.dart
@@ -1,125 +1,41 @@
-import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
 import 'package:test/test.dart';
-import 'package:trello_sdk/src/sdk/boards/board_client.dart';
 import 'package:trello_sdk/src/sdk/boards/board_models.dart';
-import 'package:trello_sdk/src/sdk/errors.dart';
 import 'package:trello_sdk/src/sdk/lists/list_models.dart';
 
 import '../../cli/cli_commons.dart';
-import '../../mocks/dio_mock.dart';
 import '../sdk_commons.dart';
 
 void main() {
-  group('getLists', () {
-    group('success', () {
-      //given
-      var boardId = 'my-board-id';
-      var client = TestTrelloClient(responses: [
-        createResponse(body: [
+  group('board lists', () {
+    apiTest<List<TrelloList>>(
+        apiCall: (client) =>
+            client.trelloClient.board(BoardId('my-board-id')).getLists(),
+        expectedMethod: 'GET',
+        expectedPath: '/1/boards/my-board-id/lists',
+        expectedQueryParameters: {
+          'cards': 'all',
+          'card_fields': 'all',
+          'filter': 'all',
+          'fields': 'all'
+        },
+        existingResourceResponse: createResponse(body: [
           {
             'id': 'my-list-id',
             'name': 'my-list-name',
             'closed': true,
-            'idBoard': boardId,
+            'idBoard': 'my-board-id',
             'pos': 2000,
             'subscribed': true,
           }
         ]),
-      ]);
-      var boardClient = client.trelloClient.board(BoardId(boardId));
-      late Either<Failure, TrelloList> firstList;
-
-      //when
-      setUpAll(() async => firstList = await getFirstList(boardClient));
-
-      //then
-      group('request', () {
-        late RequestOptions request;
-        setUpAll(() => request = client.fetchHistory[0].head);
-        test('there was one request', () => client.fetchHistory.length == 1);
-        test('method', () => request.method == 'GET');
-        test('path', () => request.path == '/1/foo');
-        test(
-            'query parameters',
-            () =>
-                request.queryParameters ==
-                {
-                  'foo',
-                  'bar',
-                });
-      });
-      group('response', () {
-        test(
-            'board id',
-            () async => verify<TrelloList>(
-                firstList, (r) => expect(r.id, ListId('my-list-id'))));
-        test(
-            'board name',
-            () async => verify<TrelloList>(
-                firstList, (r) => expect(r.name, 'my-list-name')));
-        test(
-            'board closed',
-            () async =>
-                verify<TrelloList>(firstList, (r) => expect(r.closed, true)));
-        test(
-            'board idBoard',
-            () async => verify<TrelloList>(
-                firstList, (r) => expect(r.idBoard, BoardId(boardId))));
-        test(
-            'board pos',
-            () async =>
-                verify<TrelloList>(firstList, (r) => expect(r.pos, 2000)));
-        test(
-            'board subscribed',
-            () async => verify<TrelloList>(
-                firstList, (r) => expect(r.subscribed, true)));
-      });
-    });
-    group('http client failure', () {
-      //given
-      var boardId = 'my-board-id';
-      var client = TestTrelloClient(responses: [
-        createResponse(statusCode: 404, body: {}),
-      ]);
-      var boardClient = client.trelloClient.board(BoardId(boardId));
-      late Either<Failure, List<TrelloList>> response;
-
-      //when
-      setUpAll(() async => response = await boardClient.getLists().run());
-
-      //then
-      group('request', () {
-        late RequestOptions request;
-        setUpAll(() => request = client.fetchHistory[0].head);
-        test('there was one request', () => client.fetchHistory.length == 1);
-        test('method', () => request.method == 'GET');
-        test('path', () => request.path == '/1/foo');
-        test(
-            'query parameters',
-            () =>
-                request.queryParameters ==
-                {
-                  'foo': 'bar',
-                });
-      });
-      group('response', () {
-        test(
-            'status code',
-            () async => response.fold(
-                  (l) => expect(
-                      l.toString(),
-                      ResourceNotFoundFailure(
-                              resource:
-                                  '/1/boards/my-board-id/lists - {boardId: my-board-id}')
-                          .toString()),
-                  (r) => fail('should have failed'),
-                ));
-      });
-    });
+        responseValues: [
+          testValue('id', (r) => r[0].id, ListId('my-list-id')),
+          testValue('name', (r) => r[0].name, 'my-list-name'),
+          testValue('closed', (r) => r[0].closed, isTrue),
+          testValue('idBoard', (r) => r[0].idBoard, BoardId('my-board-id')),
+          testValue('pos', (r) => r[0].pos, 2000),
+          testValue('subscribed', (r) => r[0].subscribed, isTrue),
+        ],
+        additionalContext: {'boardId': 'my-board-id'});
   });
 }
-
-Future<Either<Failure, TrelloList>> getFirstList(
-        BoardClient boardClient) async =>
-    boardClient.getLists().map((list) => list[0]).run();
--- test/src/sdk/sdk_commons.dart
+++ test/src/sdk/sdk_commons.dart
@@ -1,7 +1,110 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 import 'package:trello_sdk/src/sdk/errors.dart';
+import 'package:trello_sdk/src/sdk/extensions/list_extensions.dart';
+import 'package:trello_sdk/src/sdk/fp/fp_task_either.dart';
+
+import '../cli/cli_commons.dart';
+import '../mocks/dio_mock.dart';
 
 void verify<V>(Either<Failure, V> value, void Function(V) fn) {
   value.fold((l) => fail('should have succeeded'), (r) => fn(r));
+}
+
+class TestResponseValue<T> {
+  TestResponseValue(this._name, this._extract, this._expectedValue);
+
+  final String _name;
+  final dynamic Function(T) _extract;
+  final dynamic _expectedValue;
+
+  String get name => _name;
+  dynamic Function(T) get get => _extract;
+  dynamic get expected => _expectedValue;
+}
+
+TestResponseValue<T> testValue<T>(
+        String name, dynamic Function(T) get, dynamic expected) =>
+    TestResponseValue(name, get, expected);
+
+void apiTest<T>({
+  required TaskEither<Failure, T> Function(TestTrelloClient) apiCall,
+  required String expectedMethod,
+  required String expectedPath,
+  required Map<String, String> expectedQueryParameters,
+  required ResponseBody existingResourceResponse,
+  required List<TestResponseValue<T>> responseValues,
+  required Map<String, String> additionalContext,
+}) {
+  group('exists', () {
+    //given
+    var client = TestTrelloClient(responses: [existingResourceResponse]);
+    late final Either<Failure, T> response;
+
+    //when
+    setUpAll(() async => response = await apiCall(client).run());
+
+    //then
+    group('request', () {
+      var request;
+      setUpAll(() {
+        if (client.fetchHistory.isNotEmpty) {
+          var firstFetch = client.fetchHistory.head;
+          if (firstFetch != null) {
+            request = firstFetch.head;
+          }
+        }
+      });
+      test(
+          'there was one request', () => expect(client.fetchHistory.length, 1));
+      test('got first request', () => expect(request, isNotNull));
+      test('method', () => expect(request.method, expectedMethod));
+      test('path', () => expect(request.path, expectedPath));
+      test('query parameters',
+          () => expect(request.queryParameters, expectedQueryParameters));
+    });
+    group('response', () {
+      responseValues.forEach((element) => test('property ${element.name}', () {
+            verify<T>(response,
+                (resource) => expect(element.get(resource), element.expected));
+          }));
+    });
+  });
+  group('not found', () {
+    //given
+    var missingResponse = createResponse(statusCode: 404, body: {});
+    var client = TestTrelloClient(responses: [missingResponse]);
+    late final Either<Failure, T> response;
+
+    //when
+    setUpAll(() async => response = await apiCall(client).run());
+
+    //then
+    group('request', () {
+      var request;
+      test(
+          'there was one request', () => expect(client.fetchHistory.length, 1));
+      setUp(() {
+        request = client.fetchHistory.head!.head;
+      });
+      test('got first request', () => expect(request, isNotNull));
+      test('method', () => expect(request.method, expectedMethod));
+      test('path', () => expect(request.path, expectedPath));
+      test('query parameters',
+          () => expect(request.queryParameters, expectedQueryParameters));
+    });
+    group('response', () {
+      test(
+          'status code',
+          () async => response.fold(
+                (l) => expect(
+                    l.toString(),
+                    ResourceNotFoundFailure(resource: expectedPath)
+                        .withContext(additionalContext)
+                        .toString()),
+                (r) => fail('should have failed'),
+              ));
+    });
+  });
 }
