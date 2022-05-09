b11c86981e6a3f431f51954d70ddfc5610a3fcc9
Paul Campbell <pcampbell@kemitix.net>
2022-05-09 18:30:58 +0100

    coverage: clean up before creating coverage

--- coverage/create.sh
+++ coverage/create.sh
@@ -4,6 +4,7 @@ set -e
 
 cd $(dirname "$0")/..
 
+rm -r coverage/iconv.info coverage/report coverage/test 2>/dev/null && true
 dart run test --coverage=./coverage
 dart pub global activate coverage
 dart pub global run coverage:format_coverage --packages=.packages --report-on=lib --lcov -o ./coverage/lcov.info -i ./coverage
