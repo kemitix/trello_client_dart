49c9f6a17663481e2db31933d5a93c73e4819501
Paul Campbell <pcampbell@kemitix.net>
2022-05-10 20:16:20 +0100

    cards_client_test: add tests for attachments

--- lib/src/sdk/http_client.dart
+++ lib/src/sdk/http_client.dart
@@ -141,8 +141,7 @@ class DioHttpClient extends HttpClient {
     Map<String, String>? headers,
     void Function(int, int)? onReceiveProgress,
   }) =>
-      //TODO use _dio.downloadUri() - remove _dioDownload entirely
-      TaskEither.fromTask(Task(() => _dioDownloader.get(
+      attemptTask(() => _dioDownloader.get(
             path, onReceiveProgress: onReceiveProgress,
             queryParameters: queryParameters,
             //Received data with List<int>
@@ -154,12 +153,16 @@ class DioHttpClient extends HttpClient {
               },
               headers: headers,
             ),
-          ))).bimap(
-        (l) =>
-            HttpClientFailure(message: '(download) GET $path - ${l.message}'),
-        (r) {
-          return _fileWriter(fileName, r.data);
+          )).bimap(
+        (l) {
+          if (l.runtimeType == DioError &&
+              (l as DioError).response != null &&
+              l.response!.statusCode == 404) {
+            return ResourceNotFoundFailure(resource: path);
+          }
+          return HttpClientFailure(message: '(download) GET $path');
         },
+        (r) => _fileWriter(fileName, r.data),
       );
 
   Future<void> defaultFileWriter(FileName fileName, dynamic data) {
@@ -169,39 +172,39 @@ class DioHttpClient extends HttpClient {
   }
 }
 
-class CurlLoggerDioInterceptor extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    print('query parameters: ${options.queryParameters}');
-    print(dio2curl(options));
-    handler.next(options);
-  }
-
-  String dio2curl<T>(RequestOptions requestOptions) {
-    var curl = '';
-    // Add PATH + REQUEST_METHOD
-    curl += "curl --request ${requestOptions.method} ";
-    // Add query parameters
-    var params = <String>[];
-    requestOptions.queryParameters.forEach((key, value) {
-      params.add('$key=$value');
-    });
-    var queryParameters = '';
-    if (params.isNotEmpty) queryParameters += '?' + params.join('&');
-    // assemble url
-    curl += '"${requestOptions.baseUrl}${requestOptions.path}$queryParameters"';
-    // Include headers
-    for (var key in requestOptions.headers.keys) {
-      curl += ' -H \'$key: ${requestOptions.headers[key]}\'';
-    }
-    // Include data if there is data
-    if (requestOptions.data != null) {
-      curl += ' --data-binary \'${requestOptions.data}\'';
-    }
-    curl += ' --insecure'; //bypass https verification
-    return curl;
-  }
-}
+// class CurlLoggerDioInterceptor extends Interceptor {
+//   @override
+//   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+//     print('query parameters: ${options.queryParameters}');
+//     print(dio2curl(options));
+//     handler.next(options);
+//   }
+//
+//   String dio2curl<T>(RequestOptions requestOptions) {
+//     var curl = '';
+//     // Add PATH + REQUEST_METHOD
+//     curl += "curl --request ${requestOptions.method} ";
+//     // Add query parameters
+//     var params = <String>[];
+//     requestOptions.queryParameters.forEach((key, value) {
+//       params.add('$key=$value');
+//     });
+//     var queryParameters = '';
+//     if (params.isNotEmpty) queryParameters += '?' + params.join('&');
+//     // assemble url
+//     curl += '"${requestOptions.baseUrl}${requestOptions.path}$queryParameters"';
+//     // Include headers
+//     for (var key in requestOptions.headers.keys) {
+//       curl += ' -H \'$key: ${requestOptions.headers[key]}\'';
+//     }
+//     // Include data if there is data
+//     if (requestOptions.data != null) {
+//       curl += ' --data-binary \'${requestOptions.data}\'';
+//     }
+//     curl += ' --insecure'; //bypass https verification
+//     return curl;
+//   }
+// }
 
 class DioHttpResponse<T> extends HttpResponse<T> {
   final Response<T> _response;
--- test/src/sdk/cards/cards_client_test.dart
+++ test/src/sdk/cards/cards_client_test.dart
@@ -8,179 +8,224 @@ import '../sdk_commons.dart';
 
 void main() {
   //get
-  group('card get', () {
-    var now = DateTime.now();
-    var dateLastActive = now.add(Duration(seconds: 1));
-    var dateDue = now.add(Duration(minutes: 1));
-    var myIdShort = 123;
-    var myPos = 234.123;
-    apiTest<TrelloCard>(
-        apiCall: (client) =>
-            client.trelloClient.card(CardId('my-card-id')).get(),
-        expectedMethod: 'GET',
-        expectedPath: '/1/cards/my-card-id',
-        expectedQueryParameters: {'fields': 'all'},
-        existingResourceResponse: createResponse(body: {
-          'id': 'my-card-id',
-          'badges': {
-            // 'attachmentsByType': {
-            //   'trello': {
-            //     'board': 123,
-            //     'card': 234,
-            //   }
-            // },
-            'location': true,
-            'votes': 345,
-            'viewingMemberVoted': true,
-            'subscribed': true,
-            'fogbugz': 'my-fogbugz',
-            'checkItems': 567,
-            'checkItemsChecked': 456,
-            'comments': 678,
-            'attachments': 789,
-            'description': true,
-            'due': dateDue.toString(),
+  group(
+      'card get 1 of 2',
+      () => apiTest<TrelloCard>(
+          apiCall: (client) =>
+              client.trelloClient.card(CardId('my-card-id')).get(),
+          expectedMethod: 'GET',
+          expectedPath: '/1/cards/my-card-id',
+          expectedQueryParameters: {'fields': 'all'},
+          existingResourceResponse: createResponse(body: {
+            'id': 'my-card-id',
+            'badges': {
+              // 'attachmentsByType': {
+              //   'trello': {
+              //     'board': 123,
+              //     'card': 234,
+              //   }
+              // },
+              'location': true,
+              'votes': 345,
+              'viewingMemberVoted': true,
+              'subscribed': true,
+              'fogbugz': 'my-fogbugz',
+              'checkItems': 567,
+              'checkItemsChecked': 456,
+              'comments': 678,
+              'attachments': 789,
+              'description': true,
+              'due': DateTime(2022, 04, 05).toString(),
+              'dueComplete': true,
+            },
+            'name': 'my-card-name',
+            'closed': true,
+            'dateLastActivity': DateTime(2022, 04, 07).toString(),
+            'due': DateTime(2022, 04, 06).toString(),
             'dueComplete': true,
-          },
-          'name': 'my-card-name',
-          'closed': true,
-          'dateLastActivity': dateLastActive.toString(),
-          'due': dateDue.toString(),
-          'dueComplete': true,
-          'idAttachmentCover': 'my-id-attachment-cover',
-          'idBoard': 'my-id-board',
-          'idChecklists': ['my-id-checklist-1', 'my-id-checklist-2'],
-          'idLabels': ['my-id-labels-1', 'my-id-labels-2'],
-          'idList': 'my-id-list',
-          'idMembers': ['my-id-members-1', 'my-id-members-2'],
-          'idMembersVoted': ['my-id-members-voted-1', 'my-id-members-voted-2'],
-          'idShort': myIdShort,
-          'labels': [
-            {
-              'id': 'my-label-id-1',
-              'idBoard': 'my-label-id-board-1',
-              'name': 'my-label-name-1',
-              'color': 'my-label-color-1'
-            },
-            {
-              'id': 'my-label-id-2',
-              'idBoard': 'my-label-id-board-2',
-              'name': 'my-label-name-2',
-              'color': 'my-label-color-2'
-            },
+            'idAttachmentCover': 'my-id-attachment-cover',
+            'idBoard': 'my-id-board',
+            'idChecklists': ['my-id-checklist-1', 'my-id-checklist-2'],
+            'idLabels': ['my-id-labels-1', 'my-id-labels-2'],
+            'idList': 'my-id-list',
+            'idMembers': ['my-id-members-1', 'my-id-members-2'],
+            'idMembersVoted': [
+              'my-id-members-voted-1',
+              'my-id-members-voted-2'
+            ],
+            'idShort': 123,
+            'labels': [
+              {
+                'id': 'my-label-id-1',
+                'idBoard': 'my-label-id-board-1',
+                'name': 'my-label-name-1',
+                'color': 'my-label-color-1'
+              },
+              {
+                'id': 'my-label-id-2',
+                'idBoard': 'my-label-id-board-2',
+                'name': 'my-label-name-2',
+                'color': 'my-label-color-2'
+              },
+            ],
+            'manualCoverAttachment': true,
+            'pos': 234.123,
+            'shortLink': 'my-short-link',
+            'shortUrl': 'my-short-url',
+            'subscribed': true,
+            'url': 'my-url',
+            'address': 'my-address',
+            'locationName': 'my-location-name',
+            'coordinates': '12.34,45.67',
+          }),
+          expectedHeaders: {},
+          responseValues: [
+            testValue('id', (r) => r.id, CardId('my-card-id')),
+            testValue('closed', (r) => r.closed, isTrue),
+            testValue('dateLastActivity', (r) => r.dateLastActivity,
+                DateTime(2022, 04, 07)),
+            testValue('due', (r) => r.due, DateTime(2022, 04, 06)),
+            testValue('dueComplete', (r) => r.dueComplete, isTrue),
+            testValue('idAttachmentCover', (r) => r.idAttachmentCover,
+                'my-id-attachment-cover'),
+            testValue('idBoard', (r) => r.idBoard, 'my-id-board'),
+            testValue('idChecklists', (r) => r.idChecklists,
+                ['my-id-checklist-1', 'my-id-checklist-2']),
+            testValue('idLabels', (r) => r.idLabels,
+                ['my-id-labels-1', 'my-id-labels-2']),
+            testValue('idList', (r) => r.idList, 'my-id-list'),
+            testValue('idMembers', (r) => r.idMembers,
+                ['my-id-members-1', 'my-id-members-2']),
+            testValue('idMembersVoted', (r) => r.idMembersVoted,
+                ['my-id-members-voted-1', 'my-id-members-voted-2']),
+            testValue('idShort', (r) => r.idShort, 123),
+            testValue('labels', (r) => r.labels, [
+              CardLabel('my-label-id-1', 'my-label-id-board-1',
+                  'my-label-name-1', 'my-label-color-1'),
+              CardLabel('my-label-id-2', 'my-label-id-board-2',
+                  'my-label-name-2', 'my-label-color-2'),
+            ]),
+            testValue('manualCoverAttachment', (r) => r.manualCoverAttachment,
+                isTrue),
+            testValue('name', (r) => r.name, 'my-card-name'),
+            testValue('pos', (r) => r.pos, 234.123),
+            testValue('shortLink', (r) => r.shortLink, 'my-short-link'),
+            testValue('shortUrl', (r) => r.shortUrl, 'my-short-url'),
+            testValue('subscribed', (r) => r.subscribed, isTrue),
+            testValue('url', (r) => r.url, 'my-url'),
+            testValue('address', (r) => r.address, 'my-address'),
+            testValue(
+                'locationName', (r) => r.locationName, 'my-location-name'),
+            testValue('coordinates', (r) => r.coordinates,
+                CardCoordinates(latitude: 12.34, longitude: 45.67)),
+            testValue(
+                'badges',
+                (r) => r.badges,
+                CardBadges(345, true, true, 'my-fogbugz', 567, 456, 678, 789,
+                    true, DateTime(2022, 04, 05), true)),
           ],
-          'manualCoverAttachment': true,
-          'pos': myPos,
-          'shortLink': 'my-short-link',
-          'shortUrl': 'my-short-url',
-          'subscribed': true,
-          'url': 'my-url',
-          'address': 'my-address',
-          'locationName': 'my-location-name',
-          'coordinates': '12.34,45.67',
-        }),
-        expectedHeaders: {},
-        responseValues: [
-          testValue('id', (r) => r.id, CardId('my-card-id')),
-          testValue('closed', (r) => r.closed, isTrue),
-          testValue(
-              'dateLastActivity', (r) => r.dateLastActivity, dateLastActive),
-          testValue('due', (r) => r.due, dateDue),
-          testValue('dueComplete', (r) => r.dueComplete, isTrue),
-          testValue('idAttachmentCover', (r) => r.idAttachmentCover,
-              'my-id-attachment-cover'),
-          testValue('idBoard', (r) => r.idBoard, 'my-id-board'),
-          testValue('idChecklists', (r) => r.idChecklists,
-              ['my-id-checklist-1', 'my-id-checklist-2']),
-          testValue('idLabels', (r) => r.idLabels,
-              ['my-id-labels-1', 'my-id-labels-2']),
-          testValue('idList', (r) => r.idList, 'my-id-list'),
-          testValue('idMembers', (r) => r.idMembers,
-              ['my-id-members-1', 'my-id-members-2']),
-          testValue('idMembersVoted', (r) => r.idMembersVoted,
-              ['my-id-members-voted-1', 'my-id-members-voted-2']),
-          testValue('idShort', (r) => r.idShort, myIdShort),
-          testValue('labels', (r) => r.labels, [
-            CardLabel('my-label-id-1', 'my-label-id-board-1', 'my-label-name-1',
-                'my-label-color-1'),
-            CardLabel('my-label-id-2', 'my-label-id-board-2', 'my-label-name-2',
-                'my-label-color-2'),
-          ]),
-          testValue(
-              'manualCoverAttachment', (r) => r.manualCoverAttachment, isTrue),
-          testValue('name', (r) => r.name, 'my-card-name'),
-          testValue('pos', (r) => r.pos, myPos),
-          testValue('shortLink', (r) => r.shortLink, 'my-short-link'),
-          testValue('shortUrl', (r) => r.shortUrl, 'my-short-url'),
-          testValue('subscribed', (r) => r.subscribed, isTrue),
-          testValue('url', (r) => r.url, 'my-url'),
-          testValue('address', (r) => r.address, 'my-address'),
-          testValue('locationName', (r) => r.locationName, 'my-location-name'),
-          testValue('coordinates', (r) => r.coordinates,
-              CardCoordinates(latitude: 12.34, longitude: 45.67)),
-          testValue(
-              'badges',
-              (r) => r.badges,
-              CardBadges(345, true, true, 'my-fogbugz', 567, 456, 678, 789,
-                  true, dateDue, true)),
-        ],
-        additionalContext: {});
-    apiTest<TrelloCard>(
-        apiCall: (client) =>
-            client.trelloClient.card(CardId('my-card-id')).get(),
-        expectedMethod: 'GET',
-        expectedPath: '/1/cards/my-card-id',
-        expectedQueryParameters: {'fields': 'all'},
-        existingResourceResponse: createResponse(body: {
-          'coordinates': {'latitude': 12.34, 'longitude': 45.67},
-        }),
-        expectedHeaders: {},
-        responseValues: [
-          testValue('coordinates', (r) => r.coordinates,
-              CardCoordinates(latitude: 12.34, longitude: 45.67)),
-        ],
-        additionalContext: {});
-  });
+          additionalContext: {}));
+  group(
+      'card get 2 of 2',
+      () => apiTest<TrelloCard>(
+          apiCall: (client) =>
+              client.trelloClient.card(CardId('my-card-id')).get(),
+          expectedMethod: 'GET',
+          expectedPath: '/1/cards/my-card-id',
+          expectedQueryParameters: {'fields': 'all'},
+          existingResourceResponse: createResponse(body: {
+            'coordinates': {'latitude': 12.34, 'longitude': 45.67},
+          }),
+          expectedHeaders: {},
+          responseValues: [
+            testValue('coordinates', (r) => r.coordinates,
+                CardCoordinates(latitude: 12.34, longitude: 45.67)),
+          ],
+          additionalContext: {}));
   //put
-  group('card put', () {
-    var body = {'name': 'my-new-card-name', 'desc': 'my-new-card-desc'};
-    apiTest<TrelloCard>(
-        apiCall: (client) =>
-            client.trelloClient.card(CardId('my-card-id')).put(body),
-        expectedMethod: 'PUT',
-        expectedPath: '/1/cards/my-card-id',
-        expectedHeaders: {
-          Headers.contentTypeHeader: Headers.formUrlEncodedContentType,
-          Headers.contentLengthHeader: '43',
-        },
-        expectedQueryParameters: {},
-        existingResourceResponse: createResponse(body: {
-          'id': 'my-card-id',
-          'name': 'my-new-card-name',
-          'desc': 'my-new-card-desc',
-        }),
-        responseValues: [
-          testValue('id', (r) => r.id, CardId('my-card-id')),
-          testValue('name', (r) => r.name, 'my-new-card-name'),
-          testValue('desc', (r) => r.desc, 'my-new-card-desc'),
-        ],
-        additionalContext: {});
-  });
+  group(
+      'card put',
+      () => apiTest<TrelloCard>(
+          apiCall: (client) => client.trelloClient
+              .card(CardId('my-card-id'))
+              .put({'name': 'my-new-card-name', 'desc': 'my-new-card-desc'}),
+          expectedMethod: 'PUT',
+          expectedPath: '/1/cards/my-card-id',
+          expectedHeaders: {
+            Headers.contentTypeHeader: Headers.formUrlEncodedContentType,
+            Headers.contentLengthHeader: '43',
+          },
+          expectedQueryParameters: {},
+          existingResourceResponse: createResponse(body: {
+            'id': 'my-card-id',
+            'name': 'my-new-card-name',
+            'desc': 'my-new-card-desc',
+          }),
+          responseValues: [
+            testValue('id', (r) => r.id, CardId('my-card-id')),
+            testValue('name', (r) => r.name, 'my-new-card-name'),
+            testValue('desc', (r) => r.desc, 'my-new-card-desc'),
+          ],
+          additionalContext: {}));
   //addMember
-  group('card addMember', () {
-    apiTest(
-      apiCall: (client) => client.trelloClient
-          .card(CardId('my-card-id'))
-          .addMember(MemberId('my-member-id')),
-      expectedMethod: 'POST',
-      expectedPath: '/1/cards/my-card-id/idMembers',
-      expectedHeaders: {Headers.contentTypeHeader: Headers.jsonContentType},
-      expectedQueryParameters: {'value': 'my-member-id'},
-      existingResourceResponse: createResponse(body: {}),
-      responseValues: [],
-      additionalContext: {},
-    );
-  });
+  group(
+      'card addMember',
+      () => apiTest(
+            apiCall: (client) => client.trelloClient
+                .card(CardId('my-card-id'))
+                .addMember(MemberId('my-member-id')),
+            expectedMethod: 'POST',
+            expectedPath: '/1/cards/my-card-id/idMembers',
+            expectedHeaders: {
+              Headers.contentTypeHeader: Headers.jsonContentType
+            },
+            expectedQueryParameters: {'value': 'my-member-id'},
+            existingResourceResponse: createResponse(body: {}),
+            responseValues: [],
+            additionalContext: {},
+          ));
   //attachments
+  group(
+      'attachments',
+      () => apiTest<List<TrelloAttachment>>(
+            apiCall: (client) =>
+                client.trelloClient.card(CardId('my-card-id')).attachments(),
+            expectedMethod: 'GET',
+            expectedPath: '/1/cards/my-card-id/attachments',
+            expectedHeaders: {
+              'Authorization':
+                  'OAuth oauth_consumer_key="_key", oauth_token="_token"'
+            },
+            expectedQueryParameters: {'filter': 'false', 'fields': 'all'},
+            existingResourceResponse: createResponse(body: [
+              {
+                'id': 'my-id',
+                'bytes': 'my-bytes',
+                'date': DateTime(2022, 04, 03).toString(),
+                'edgeColor': 'my-edge-color',
+                'idMember': 'my-member-id',
+                'isUpload': true,
+                'mimeType': 'my-mime-type',
+                'name': 'my-name',
+                'previews': ['my-preview-1', 'my-preview-2'],
+                'url': 'my-url',
+                'pos': 123.456,
+              },
+            ]),
+            responseValues: [
+              testValue('id', (r) => r[0].id, 'my-id'),
+              testValue('bytes', (r) => r[0].bytes, 'my-bytes'),
+              testValue('date', (r) => r[0].date, DateTime(2022, 04, 03)),
+              testValue('edgeColor', (r) => r[0].edgeColor, 'my-edge-color'),
+              testValue('idMember', (r) => r[0].idMember, 'my-member-id'),
+              testValue('isUpload', (r) => r[0].isUpload, isTrue),
+              testValue('mimeType', (r) => r[0].mimeType, 'my-mime-type'),
+              testValue('previews', (r) => r[0].previews,
+                  ['my-preview-1', 'my-preview-2']),
+              testValue('url', (r) => r[0].url, 'my-url'),
+              testValue('pos', (r) => r[0].pos, 123.456),
+            ],
+            additionalContext: {},
+          ));
   //attachment
 }
