90902d9dc64def9261b0b2a19205f059cba70387
Paul Campbell <pcampbell@kemitix.net>
2022-05-10 18:56:36 +0100

    more tests in card_client_test

--- lib/src/sdk/cards/card_models.dart
+++ lib/src/sdk/cards/card_models.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'package:equatable/equatable.dart';
 
 import '../client.dart';
 import '../trello_object_model.dart';
@@ -7,44 +7,86 @@ class TrelloCard extends TrelloObject<CardFields> {
   TrelloCard(source, List<CardFields> fields)
       : super(source, fields, all: fields.contains(CardFields.all));
 
-  factory TrelloCard.fromJson(Map<String, dynamic> json) =>
-      TrelloCard(json, [CardFields.all]);
+  // factory TrelloCard.fromJson(Map<String, dynamic> json) =>
+  //     TrelloCard(json, [CardFields.all]);
 
   CardId get id => CardId(getValue(CardFields.id));
-  CardBadges get badges => getValue(CardFields.badges);
+
+  CardBadges get badges {
+    var e = (getValue(CardFields.badges) as Map<String, dynamic>);
+    return CardBadges(
+        e['votes'],
+        e['viewingMemberVoted'],
+        e['subscribed'],
+        e['fogbugz'],
+        e['checkItems'],
+        e['checkItemsChecked'],
+        e['comments'],
+        e['attachments'],
+        e['description'],
+        DateTime.parse(e['due']),
+        e['dueComplete']);
+  }
+
   //List<?> get checkItemStates => getValue(CardFields.checkItemStates);
   bool get closed => getValue(CardFields.closed);
+
   DateTime? get dateLastActivity =>
       toDateTime(getValue(CardFields.dateLastActivity));
+
   String get desc => getValue(CardFields.desc);
+
   //? get descData => getValue(CardFields.descData);
   DateTime? get due => toDateTime(getValue(CardFields.due));
+
   bool get dueComplete => getValue(CardFields.dueComplete);
+
   String get idAttachmentCover => getValue(CardFields.idAttachmentCover);
+
   String get idBoard => getValue(CardFields.idBoard);
+
   List<String> get idChecklists =>
       getValueAsListString(CardFields.idChecklists);
+
   List<String> get idLabels => getValueAsListString(CardFields.idLabels);
+
   String get idList => getValue(CardFields.idList);
+
   List<String> get idMembers => getValueAsListString(CardFields.idMembers);
+
   List<String> get idMembersVoted =>
       getValueAsListString(CardFields.idMembersVoted);
+
   int get idShort => getValue(CardFields.idShort);
-  List<CardLabel> get labels => getValue(CardFields.labels);
+
+  List<CardLabel> get labels => (getValue(CardFields.labels) as List<dynamic>)
+      .map((e) => CardLabel(e['id'], e['idBoard'], e['name'], e['color']))
+      .toList();
+
   bool get manualCoverAttachment => getValue(CardFields.manualCoverAttachment);
+
   String get name => getValue(CardFields.name);
-  Float get pos => getValue(CardFields.pos);
+
+  double get pos => getValue(CardFields.pos);
+
   String get shortLink => getValue(CardFields.shortLink);
+
   String get shortUrl => getValue(CardFields.shortUrl);
+
   bool get subscribed => getValue(CardFields.subscribed);
+
   String get url => getValue(CardFields.url);
+
   String get address => getValue(CardFields.address);
+
   String get locationName => getValue(CardFields.locationName);
-  CardCoordinates get coodinates {
-    var value = raw[CardFields.coodinates.name];
+
+  CardCoordinates get coordinates {
+    var value = getValue(CardFields.coordinates);
     if (value is String) {
       List<String> split = value.split(',');
-      return CardCoordinates(latitude: split[0], longitude: split[1]);
+      return CardCoordinates(
+          latitude: double.parse(split[0]), longitude: double.parse(split[1]));
     }
     return CardCoordinates(
         latitude: value['latitude'], longitude: value['longitude']);
@@ -64,24 +106,29 @@ class FileName extends StringValue {
 }
 
 // Field vale may come from Trello as 'latitude,longitude'
-class CardCoordinates {
-  late Float latitude;
-  late Float longitude;
+class CardCoordinates with EquatableMixin {
+  late double latitude;
+  late double longitude;
 
-  CardCoordinates({required latitude, required longitude}) {
-    this.latitude = latitude;
-    this.longitude = longitude;
-  }
+  CardCoordinates({required this.latitude, required this.longitude});
+
+  @override
+  List<Object?> get props => [latitude, longitude];
 }
 
-class CardLabel {
+class CardLabel with EquatableMixin {
   late String id;
   late String idBoard;
   late String name;
   late String color;
+
+  CardLabel(this.id, this.idBoard, this.name, this.color);
+
+  @override
+  List<Object?> get props => [id, idBoard, name, color];
 }
 
-class CardBadges {
+class CardBadges with EquatableMixin {
   late int votes;
   late bool viewingMemberVoted;
   late bool subscribed;
@@ -93,6 +140,34 @@ class CardBadges {
   late bool description;
   late DateTime due;
   late bool dueComplete;
+
+  CardBadges(
+      this.votes,
+      this.viewingMemberVoted,
+      this.subscribed,
+      this.fogbugz,
+      this.checkItems,
+      this.checkItemsChecked,
+      this.comments,
+      this.attachments,
+      this.description,
+      this.due,
+      this.dueComplete);
+
+  @override
+  List<Object?> get props => [
+        votes,
+        viewingMemberVoted,
+        subscribed,
+        fogbugz,
+        checkItems,
+        checkItemsChecked,
+        comments,
+        attachments,
+        description,
+        due,
+        dueComplete
+      ];
 }
 
 enum CardFields {
@@ -124,7 +199,7 @@ enum CardFields {
   url,
   address,
   locationName,
-  coodinates,
+  coordinates,
 }
 
 enum CardFilter {
@@ -140,9 +215,9 @@ class TrelloAttachment extends TrelloObject<AttachmentFields> {
 
   String get url => getValue(AttachmentFields.url);
 
-  int get bytes => getValue(AttachmentFields.bytes);
+// int get bytes => getValue(AttachmentFields.bytes);
 
-  //TODO add field getters
+//TODO add field getters
 }
 
 enum AttachmentFields {
