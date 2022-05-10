d2a83e01bb05f3cfaf87b8c047a998d06aa51472
Paul Campbell <pcampbell@kemitix.net>
2022-05-10 20:16:20 +0100

    cards_client_test: add tests for attachments

--- lib/src/sdk/cards/card_models.dart
+++ lib/src/sdk/cards/card_models.dart
@@ -213,11 +213,27 @@ class TrelloAttachment extends TrelloObject<AttachmentFields> {
   TrelloAttachment(source, List<AttachmentFields> fields)
       : super(source, fields);
 
+  String get id => getValue(AttachmentFields.id);
+
+  String get bytes => getValue(AttachmentFields.bytes);
+
+  DateTime? get date => toDateTime(getValue(AttachmentFields.date));
+
+  String get edgeColor => getValue(AttachmentFields.edgeColor);
+
+  String get idMember => getValue(AttachmentFields.idMember);
+
+  bool get isUpload => getValue(AttachmentFields.isUpload);
+
+  String get mimeType => getValue(AttachmentFields.mimeType);
+
+  String get name => getValue(AttachmentFields.name);
+
+  List<String> get previews => getValueAsListString(AttachmentFields.previews);
+
   String get url => getValue(AttachmentFields.url);
 
-// int get bytes => getValue(AttachmentFields.bytes);
-
-//TODO add field getters
+  double get pos => getValue(AttachmentFields.pos);
 }
 
 enum AttachmentFields {
