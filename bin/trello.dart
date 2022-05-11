51e75e128649400e228a114f9718cdebd7accb3e
Paul Campbell <pcampbell@kemitix.net>
2022-05-11 17:53:59 +0100

    move and rename http_client as external/dio_client_factory

--- bin/trello.dart
+++ bin/trello.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
+import 'package:trello_sdk/src/sdk/external/dio_client_factory.dart';
 import 'package:trello_sdk/trello_cli.dart';
 
 Future<void> main(List<String> arguments) async {
   await app().run(EnvArgsEnvironment(
       platformEnvironment: Platform.environment,
       arguments: arguments,
-      clientFactory: trelloClient,
+      clientFactory: dioClientFactory,
       printer: (s) => print(s)));
 }
