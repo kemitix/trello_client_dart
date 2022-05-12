616aa029d69a6b2fa050f22e1f69f1ca502da443
Paul Campbell <pcampbell@kemitix.net>
2022-05-12 07:23:38 +0100

    coverage/create: pin coverage package at 1.2.0
    There appears to be a bug with 1.3.0 that caused the `test_with_coverage`
    command to not complete.

--- coverage/create.sh
+++ coverage/create.sh
@@ -5,7 +5,13 @@ set -ex
 cd $(dirname "$0")/..
 
 rm -r coverage/iconv.info coverage/report coverage/test 2>/dev/null && true
+
+# Coverage 1.2.0 - pinned due to possible bug in 1.3.0
 dart run test --coverage=./coverage
-dart pub global activate coverage
-dart pub global run coverage:format_coverage --packages=.packages --report-on=lib --lcov -o ./coverage/lcov.info -i ./coverage
+dart pub global activate coverage 1.2.0
+dart pub global run coverage:format_coverage 1.2.0 --packages=.packages --report-on=lib --lcov -o ./coverage/lcov.info -i ./coverage
+
+# Coverage 1.3.0 - doesn't complete - bug?
+#dart pub global run coverage:test_with_coverage
+
 genhtml -o ./coverage/report ./coverage/lcov.info
