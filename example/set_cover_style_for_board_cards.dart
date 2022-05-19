2c9421c39d9744378a70651ff114da389e439470
Paul Campbell <pcampbell@kemitix.net>
2022-05-19 20:44:57 +0100

    refactoring

--- example/set_cover_style_for_board_cards.dart
+++ example/set_cover_style_for_board_cards.dart
@@ -7,22 +7,25 @@ import 'package:trello_sdk/src/sdk/external/dio_client_factory.dart';
 import 'package:trello_sdk/trello_cli.dart';
 
 void main() async {
-  var auth = authentication(Platform.environment);
-  var client = createClient(auth);
-  var memberId = MemberId(Platform.environment['TRELLO_USERNAME']!);
-  var boards = await getMembersBoards(memberId, client);
-  var board = selectBoard(boards);
-  var lists = await getBoardLists(client, board);
-  var cards = await getCardsOnLists(client, lists);
-  var style =
-      menu(prompt: 'Cover Style to apply?', options: ['normal', 'full']);
-  var updatedCards = await updateAllCards(client, cards, style);
-  closeClient(client);
-  updatedCards.fold(
-    (l) => print('Error: $l'),
-    (r) => print('Updated ${r.length} cards'),
-  );
+  createClient(authentication(Platform.environment)).map((client) async {
+    await getMembersBoards(member(), client)
+        .then((boards) => map1either(boards, selectBoard).flatMap(id))
+        .then((board) => defer1either1(board, client, getLists))
+        .then((lists) => defer1either1(lists, client, getCardsOnLists))
+        .then((cards) =>
+            defer1either2(cards, selectStyle(), client, updateAllCards))
+        .then((cards) => cards.fold(
+              (l) => print('Error: $l'),
+              (r) => print('Updated ${r.length} cards'),
+            ));
+    client.close();
+  });
 }
+
+String selectStyle() =>
+    menu(prompt: 'Cover Style to apply?', options: ['normal', 'full']);
+
+MemberId member() => MemberId(Platform.environment['TRELLO_USERNAME']!);
 
 Either<Failure, TrelloClient> createClient(
   Either<Errors, TrelloAuthentication> auth,
@@ -33,64 +36,41 @@ Either<Failure, TrelloClient> createClient(
 
 Future<Either<Failure, List<TrelloBoard>>> getMembersBoards(
   MemberId memberId,
-  Either<Failure, TrelloClient> client,
+  TrelloClient client,
 ) async =>
-    (await Either.sequenceFuture(client.map((client) => client
-            .member(memberId)
-            .getBoards(
-                fields: [BoardFields.id, BoardFields.name],
-                filter: MemberBoardFilter.open))))
-        .flatMap(id);
+    client.member(memberId).getBoards(
+        fields: [BoardFields.id, BoardFields.name],
+        filter: MemberBoardFilter.open);
 
-Either<Failure, TrelloBoard> selectBoard(
-  Either<Failure, List<TrelloBoard>> boardList,
-) =>
-    boardList.flatMap((list) => right<Failure, List<TrelloBoard>>(list)
-            .filter(
-          (list) => list.isNotEmpty,
-          () => MemberHasNoBoardsFailure(),
-        )
-            .map((boards) {
-          var selection =
-              menu(prompt: 'Select board', options: boardNames(list));
-          return boards.where((board) => board.name == selection).first;
-        }));
+Either<Failure, TrelloBoard> selectBoard(List<TrelloBoard> boardList) =>
+    right<Failure, List<TrelloBoard>>(boardList)
+        .filter(
+      (list) => list.isNotEmpty,
+      () => MemberHasNoBoardsFailure(),
+    )
+        .map((boards) {
+      var selection =
+          menu(prompt: 'Select board', options: boardNames(boardList));
+      return boards.where((board) => board.name == selection).first;
+    });
 
 List<String> boardNames(List<TrelloBoard> list) =>
     list.map((board) => board.name).toList();
 
-Future<Either<Failure, List<TrelloList>>> getBoardLists(
-  Either<Failure, TrelloClient> client,
-  Either<Failure, TrelloBoard> selectedBoard,
+Future<Either<Failure, List<TrelloList>>> getLists(
+  TrelloBoard board,
+  TrelloClient client,
 ) async =>
-    (await Either.sequenceFuture(map2either(
-            client,
-            selectedBoard,
-            (TrelloClient client, TrelloBoard board) => client
-                .board(board.id)
-                .getLists(
-                    filter: ListFilter.all,
-                    fields: [ListFields.id, ListFields.name]))))
-        .flatMap(id);
+    client.board(board.id).getLists(
+        filter: ListFilter.all, fields: [ListFields.id, ListFields.name]);
 
 Future<Either<Failure, List<TrelloCard>>> getCardsOnLists(
-  Either<Failure, TrelloClient> client,
-  Either<Failure, List<TrelloList>> boardLists,
-) async =>
-    (await Either.sequenceFuture(map2either(
-            client,
-            boardLists,
-            (TrelloClient client, List<TrelloList> lists) async =>
-                await listOfCards(client, lists))))
-        .flatMap(id);
-
-Future<Either<Failure, List<TrelloCard>>> listOfCards(
-  TrelloClient client,
   List<TrelloList> lists,
+  TrelloClient client,
 ) async {
   var result = <TrelloCard>[];
   for (var list in lists) {
-    var cards = await getCards(client, list);
+    var cards = await getCards(list, client);
     if (cards.isLeft()) return cards;
     cards.map((cards) => result.addAll(cards));
   }
@@ -98,27 +78,15 @@ Future<Either<Failure, List<TrelloCard>>> listOfCards(
 }
 
 Future<Either<Failure, List<TrelloCard>>> getCards(
-  TrelloClient client,
   TrelloList list,
+  TrelloClient client,
 ) =>
     client.list(list.id).getCards(fields: [CardFields.id]);
 
 Future<Either<Failure, List<TrelloCard>>> updateAllCards(
-  Either<Failure, TrelloClient> client,
-  Either<Failure, List<TrelloCard>> boardCards,
-  String style,
-) =>
-    client
-        .map((client) =>
-            boardCards.map((cards) => updateCards(cards, client, style)))
-        .flatMap(id)
-        .traverseFuture(id)
-        .then((e) => e.flatMap(id));
-
-Future<Either<Failure, List<TrelloCard>>> updateCards(
   List<TrelloCard> cards,
-  TrelloClient client,
   String style,
+  TrelloClient client,
 ) async {
   var result = <TrelloCard>[];
   for (var card in cards) {
@@ -137,9 +105,6 @@ Future<Either<Failure, TrelloCard>> updateCard(
     client.card(card.id).put({
       'cover': {'size': style}
     });
-
-Either<Failure, void> closeClient(Either<Failure, TrelloClient> client) =>
-    client.map((client) => client.close());
 
 class AuthenticationFailure extends Failure {
   AuthenticationFailure(Errors errors) : super(message: errors.join(', '));
