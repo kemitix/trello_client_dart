43f30229274b240386730dbfdde88fe658c802ee
Paul Campbell <pcampbell@kemitix.net>
2022-05-22 17:18:02 +0100

    Convert card_member_remove_command_test to use cliTest

--- test/src/cli/commands/card/card_member_remove_command_test.dart
+++ test/src/cli/commands/card/card_member_remove_command_test.dart
@@ -1,94 +1,99 @@
 import 'package:test/test.dart';
-import 'package:trello_sdk/trello_cli.dart';
 
-import '../../../mocks/dio_mock.dart';
+import '../../../sdk/sdk_commons.dart';
 import '../../cli_commons.dart';
 
 void main() {
-  group('remove member from card', () {
-    //given
-    var cardId = 'my-card-id';
-    var memberId = 'my-member-id';
-    var client = TestTrelloClient(responses: [
-      createResponse(body: {
-        'id': 'my-card-id',
-        'pos': 'my-pos',
-        'name': 'my-card-name',
-        'due': '2022-05-05T09:45',
-        'idMembers': ['my-member-id'],
-      }),
-      createResponse(body: {})
-    ]);
-    var printer = FakePrinter();
-    var environment = EnvArgsEnvironment(
-      platformEnvironment: validEnvironment,
-      arguments: 'card remove-member $cardId $memberId'.split(' '),
-      clientFactory: (_) => client.trelloClient,
-      printer: printer.printer,
-    );
-
-    //when
-    setUpAll(() => app().run(environment));
-
-    //then
-    var history = client.fetchHistory;
-    test('there were two requests', () => expect(history.length, 2));
-
-    test('first request was GET', () => expect(history[0].head.method, 'GET'));
-    test('first request path',
-        () => expect(history[0].head.path, '/1/cards/$cardId'));
-    test('first request query parameters',
-        () => expect(history[0].head.queryParameters, {'fields': 'idMembers'}));
-
-    test('second request was DELETE',
-        () => expect(history[1].head.method, 'DELETE'));
-    test(
-        'second request path',
-        () => expect(
-            history[1].head.path, '/1/cards/$cardId/idMembers/my-member-id'));
-    test('second request query parameters',
-        () => expect(history[1].head.queryParameters, {}));
-
-    test('output', () => expect(printer.output, ['Removed member']));
+  var helpOutput = [
+    'Remove a Member from a Card',
+    '',
+    'Usage: trello card remove-member [arguments]',
+    '-h, --help    Print this usage information.',
+    '',
+    'Run "trello help" to see global options.'
+  ];
+  group('is a member', () {
+    cliTest(
+        arguments: 'card remove-member my-card-id my-member-id'.split(' '),
+        responses: [
+          createResponse(body: {
+            'id': 'my-card-id',
+            'pos': 'my-pos',
+            'name': 'my-card-name',
+            'due': '2022-05-05T09:45',
+            'idMembers': ['my-member-id'],
+          }),
+          createResponse(body: {})
+        ],
+        expected: CliExpectations(
+          requests: [
+            ExpectedRequest(
+              expectedMethod: 'GET',
+              expectedPath: '/1/cards/my-card-id',
+              expectedHeaders: {},
+              expectedQueryParameters: {'fields': 'idMembers'},
+            ),
+            ExpectedRequest(
+              expectedMethod: 'DELETE',
+              expectedPath: '/1/cards/my-card-id/idMembers/my-member-id',
+              expectedHeaders: {
+                'content-type': 'application/json; charset=utf-8'
+              },
+              expectedQueryParameters: {},
+            ),
+          ],
+          output: ['Removed member'],
+          help: helpOutput,
+          notFoundOutput: [
+            'ERROR: card remove-member - Failure: Resource not found: /1/cards/my-card-id'
+          ],
+          serverErrorOutput: [
+            'ERROR: card remove-member - Failure: GET /1/cards/my-card-id'
+          ],
+        ));
   });
-  group('do not remove member not on card', () {
-    //given
-    var cardId = 'my-card-id';
-    var memberId = 'my-member-id';
-    var client = TestTrelloClient(responses: [
-      createResponse(body: {
-        'id': 'my-card-id',
-        'pos': 'my-pos',
-        'name': 'my-card-name',
-        'due': '2022-05-05T09:45',
-        'idMembers': [],
-      })
-    ]);
-    var printer = FakePrinter();
-    var environment = EnvArgsEnvironment(
-      platformEnvironment: validEnvironment,
-      arguments: 'card remove-member $cardId $memberId'.split(' '),
-      clientFactory: (_) => client.trelloClient,
-      printer: printer.printer,
-    );
-
-    //when
-    setUpAll(() => app().run(environment));
-
-    //then
-    var history = client.fetchHistory;
-    test('there was one request', () => expect(history.length, 1));
-
-    test('request was GET', () => expect(history[0].head.method, 'GET'));
-    test(
-        'request path', () => expect(history[0].head.path, '/1/cards/$cardId'));
-    test('request query parameters',
-        () => expect(history[0].head.queryParameters, {'fields': 'idMembers'}));
-
-    test(
-        'output',
-        () => expect(printer.output, [
-              'ERROR: card remove-member - Failure: Can\'t Remove a Member from a Card as it is already applied'
-            ]));
+  group('is not a member', () {
+    cliTest(
+        arguments: 'card remove-member my-card-id my-member-id'.split(' '),
+        responses: [
+          createResponse(body: {
+            'id': 'my-card-id',
+            'pos': 'my-pos',
+            'name': 'my-card-name',
+            'due': '2022-05-05T09:45',
+            'idMembers': [],
+          }),
+        ],
+        testNotFound: false,
+        testServerError: false,
+        expected: CliExpectations(
+          requests: [
+            ExpectedRequest(
+                expectedMethod: 'GET',
+                expectedPath: '/1/cards/my-card-id',
+                expectedHeaders: {},
+                expectedQueryParameters: {'fields': 'idMembers'})
+          ],
+          output: [
+            'ERROR: card remove-member - Failure: Can\'t Remove a Member from a Card as it is already applied'
+          ],
+          help: helpOutput,
+        ));
   });
+  group(
+      'no member-id',
+      () => cliTest(
+          arguments: 'card remove-member my-card-id'.split(' '),
+          responses: [],
+          expected: CliExpectations(requests: [], output: [
+            'ERROR: card remove-member - Failure: Member Id not given'
+          ], help: helpOutput)));
+  group(
+      'no card-id, member-id',
+      () => cliTest(
+          arguments: 'card remove-member'.split(' '),
+          responses: [],
+          expected: CliExpectations(requests: [], output: [
+            'ERROR: card remove-member - Failure: Card Id not given'
+          ], help: helpOutput)));
 }
