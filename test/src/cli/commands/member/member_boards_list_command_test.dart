ebb2b5e6184384d79edddb9ad8ef01fb9d76406e
Paul Campbell <pcampbell@kemitix.net>
2022-05-22 18:07:19 +0100

    Convert member_boards_list_command_test to use cliTest

--- test/src/cli/commands/member/member_boards_list_command_test.dart
+++ test/src/cli/commands/member/member_boards_list_command_test.dart
@@ -1,53 +1,73 @@
 import 'package:test/test.dart';
-import 'package:trello_sdk/trello_cli.dart';
 
-import '../../../mocks/dio_mock.dart';
+import '../../../sdk/sdk_commons.dart';
 import '../../cli_commons.dart';
 
 void main() {
-  //given
-  var memberId = 'my-member-id';
-  var client = TestTrelloClient(responses: [
-    createResponse(body: [
-      // get boards
-      {
-        'id': 'my-id',
-        'name': 'my-board-name',
-        'pinned': false,
-        'closed': true,
-        'starred': false,
-        'shortUrl': 'my-short-url',
-      },
-    ]),
-  ]);
-  var printer = FakePrinter();
-  var environment = EnvArgsEnvironment(
-    platformEnvironment: validEnvironment,
-    arguments: 'member list-boards $memberId'.split(' '),
-    clientFactory: (_) => client.trelloClient,
-    printer: printer.printer,
-  );
-
-  //when
-  setUpAll(() => app().run(environment));
-
-  //then
-  var history = client.fetchHistory;
-  test('there was one request', () => expect(history.length, 1));
-  test('request was GET', () => expect(history[0].head.method, 'GET'));
-  test('request path',
-      () => expect(history[0].head.path, '/1/members/$memberId/boards'));
-  test(
-      'request query parameters',
-      () => expect(history[0].head.queryParameters, {
+  const helpOutput = [
+    'Get Boards that Member belongs to',
+    '',
+    'Usage: trello member list-boards [arguments]',
+    '-h, --help      Print this usage information.',
+    '    --fields    all or a comma-separated list of fields',
+    '                (defaults to "id,name,pinned,closed,starred,shortUrl")',
+    '',
+    'Run "trello help" to see global options.'
+  ];
+  cliTest(
+    arguments: 'member list-boards my-member-id'.split(' '),
+    responses: [
+      createResponse(body: [
+        // get boards
+        {
+          'id': 'my-id',
+          'name': 'my-board-name',
+          'pinned': false,
+          'closed': true,
+          'starred': false,
+          'shortUrl': 'my-short-url',
+        },
+      ]),
+    ],
+    expectations: CliExpectations(
+      requests: [
+        ExpectedRequest(
+          expectedMethod: 'GET',
+          expectedPath: '/1/members/my-member-id/boards',
+          expectedHeaders: {},
+          expectedQueryParameters: {
             'filter': 'all',
             'fields': 'id,name,pinned,closed,starred,shortUrl'
-          }));
-  test(
-      'output',
-      () => expect(printer.output, [
-            'id    | name          | pinned | closed | starred | shortUrl    ',
-            '------|---------------|--------|--------|---------|-------------',
-            'my-id | my-board-name | false  | true   | false   | my-short-url',
-          ]));
+          },
+        ),
+      ],
+      output: [
+        'id    | name          | pinned | closed | starred | shortUrl    ',
+        '------|---------------|--------|--------|---------|-------------',
+        'my-id | my-board-name | false  | true   | false   | my-short-url',
+      ],
+      help: helpOutput,
+      notFoundOutput: [
+        'ERROR: member list-boards - Failure: Resource not found: /1/members/my-member-id/boards'
+      ],
+      serverErrorOutput: [
+        'ERROR: member list-boards - Failure: GET /1/members/my-member-id/boards'
+      ],
+    ),
+  );
+  group(
+      'no member-id',
+      () => cliTest(
+            arguments: 'member list-boards'.split(' '),
+            responses: [],
+            testNotFound: false,
+            testServerError: false,
+            expectations: CliExpectations(
+              requests: [],
+              output: [
+                'ERROR: member list-boards - Failure: Member Id not given'
+              ],
+              help: helpOutput,
+            ),
+          ));
 }
