36d37ce1b15f1eba63f13b1e6dd6777123a7234d
Paul Campbell <pcampbell@kemitix.net>
2022-05-22 18:36:01 +0100

    example/demo_cli: use lowerCamelCase

--- example/demo_cli.dart
+++ example/demo_cli.dart
@@ -6,35 +6,34 @@ import 'package:dcli/dcli.dart';
 import 'package:trello_sdk/external/dio_client_factory.dart';
 import 'package:trello_sdk/trello_cli.dart';
 
-const String MEMBER = "kemitix";
-const String MEMBER_ID = "5d999fc87ac5a442f45cb8eb";
-const String BOARD = "5eccb96b04b4dc5666c64b7c";
-const String LIST = "5de68ade15e1dc10b583219e";
-const String CARD = "5dc1b58de65da8806ebabba8";
-const String MUTABLE_CARD = "61b32a083270082adf87ffb1";
-const String ATTACHMENT = "5dc1b58ee65da8806ebabbc7";
-const String FILE_NAME = "larkspur.rtf";
+const String member = "kemitix";
+const String memberId = "5d999fc87ac5a442f45cb8eb";
+const String boardId = "5eccb96b04b4dc5666c64b7c";
+const String listId = "5de68ade15e1dc10b583219e";
+const String cardId = "5dc1b58de65da8806ebabba8";
+const String mutableCardId = "61b32a083270082adf87ffb1";
+const String attachmentId = "5dc1b58ee65da8806ebabbc7";
+const String fileName = "larkspur.rtf";
 
 void main() async {
-  await trello('member get $MEMBER');
-  await trello('member list-boards $MEMBER');
-  await trello('board list-lists $BOARD');
-  await trello('list list-cards $LIST');
-  await trello('card get $CARD');
-  await trello('card list-attachments $CARD');
-  await trello('card get-attachment $CARD $ATTACHMENT');
-  await trello('card download-attachment $CARD $ATTACHMENT $FILE_NAME');
-  print(
-      '$FILE_NAME - ${lastModified(FILE_NAME)} - ${calculateHash(FILE_NAME)}');
-  delete(FILE_NAME);
-  await trello('card get $MUTABLE_CARD');
+  await trello('member get $member');
+  await trello('member list-boards $member');
+  await trello('board list-lists $boardId');
+  await trello('list list-cards $listId');
+  await trello('card get $cardId');
+  await trello('card list-attachments $cardId');
+  await trello('card get-attachment $cardId $attachmentId');
+  await trello('card download-attachment $cardId $attachmentId $fileName');
+  print('$fileName - ${lastModified(fileName)} - ${calculateHash(fileName)}');
+  delete(fileName);
+  await trello('card get $mutableCardId');
   await trello(
-      'card update $MUTABLE_CARD --name ${DateTime.now().toString().replaceAll(' ', '-')} --member-ids ');
-  await trello('card get $MUTABLE_CARD');
-  await trello('card add-member $MUTABLE_CARD $MEMBER_ID');
-  await trello('card get $MUTABLE_CARD');
-  await trello('card remove-member $MUTABLE_CARD $MEMBER_ID');
-  await trello('card get $MUTABLE_CARD');
+      'card update $mutableCardId --name ${DateTime.now().toString().replaceAll(' ', '-')} --member-ids ');
+  await trello('card get $mutableCardId');
+  await trello('card add-member $mutableCardId $memberId');
+  await trello('card get $mutableCardId');
+  await trello('card remove-member $mutableCardId $memberId');
+  await trello('card get $mutableCardId');
 }
 
 Future<void> trello(String command) {
