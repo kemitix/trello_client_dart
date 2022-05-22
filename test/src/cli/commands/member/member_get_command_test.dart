6ed6d0d8ff469826690ba37560b06ae94e7f8c44
Paul Campbell <pcampbell@kemitix.net>
2022-05-22 18:21:31 +0100

    Convert member_get_command_test to use cliTest

--- test/src/cli/commands/member/member_get_command_test.dart
+++ test/src/cli/commands/member/member_get_command_test.dart
@@ -1,77 +1,90 @@
 import 'package:test/test.dart';
-import 'package:trello_sdk/trello_cli.dart';
 
-import '../../../mocks/dio_mock.dart';
+import '../../../sdk/sdk_commons.dart';
 import '../../cli_commons.dart';
 
 void main() {
-  //given
-  var memberId = 'my-member-id';
-  var client = TestTrelloClient(responses: [
-    createResponse(body: {
-      'id': 'my-member-id',
-      'username': 'my-username',
-      'email': 'my-email',
-      'fullName': 'my-full-name',
-      'initials': 'my-initials',
-      'url': 'my-url',
-      'status': 'my-status',
-      'memberType': 'my-member-type',
-      'confirmed': true,
-      'bio': 'my-bio',
-    })
-  ]);
-  var printer = FakePrinter();
-  var environment = EnvArgsEnvironment(
-    platformEnvironment: validEnvironment,
-    arguments: 'member get $memberId'.split(' '),
-    clientFactory: (_) => client.trelloClient,
-    printer: printer.printer,
-  );
-
-  //when
-  setUpAll(() => app().run(environment));
-
-  //then
-  var history = client.fetchHistory;
-  test('there was one request', () => expect(history.length, 1));
-  test('request was GET', () => expect(history[0].head.method, 'GET'));
-  test('request path',
-      () => expect(history[0].head.path, '/1/members/$memberId'));
-  test(
-      'request query parameters',
-      () => expect(history[0].head.queryParameters, {
-            'boardBackgrounds': 'none',
-            'boardStars': 'false',
-            'boardsInvited': 'all',
-            'boardsInvitedFields': 'all',
-            'cars': 'none',
-            'customBoardBackground': 'none',
-            'customEmoji': 'none',
-            'customStickers': 'none',
-            'fields':
-                'id,username,email,fullName,initials,url,status,memberType,confirmed,bio',
-            'organizations': 'none',
-            'organization_fields': 'all',
-            'organization_paid_account': 'false',
-            'organizationsInvited': 'none',
-            'organizationsInvited_fields': 'all',
-            'paid_account': 'false',
-            'savedSearches': 'false',
-            'tokens': 'none',
-          }));
-  test(
-      'output',
-      () => expect(printer.output, [
-            'id         | my-member-id  ',
-            'username   | my-username   ',
-            'email      | my-email      ',
-            'fullName   | my-full-name  ',
-            'initials   | my-initials   ',
-            'url        | my-url        ',
-            'status     | my-status     ',
-            'memberType | my-member-type',
-            'confirmed  | true          ',
-            'bio        | my-bio        ',
-          ]));
+  const helpOutput = [
+    'Get a Member',
+    '',
+    'Usage: trello member get [arguments]',
+    '-h, --help      Print this usage information.',
+    '    --fields    all or a comma-separated list of fields',
+    '                (defaults to "id,username,email,fullName,initials,url,status,memberType,confirmed,bio")',
+    '',
+    'Run "trello help" to see global options.'
+  ];
+  cliTest(
+      arguments: 'member get my-member-id'.split(' '),
+      responses: [
+        createResponse(body: {
+          'id': 'my-member-id',
+          'username': 'my-username',
+          'email': 'my-email',
+          'fullName': 'my-full-name',
+          'initials': 'my-initials',
+          'url': 'my-url',
+          'status': 'my-status',
+          'memberType': 'my-member-type',
+          'confirmed': true,
+          'bio': 'my-bio',
+        })
+      ],
+      expectations: CliExpectations(
+        requests: [
+          ExpectedRequest(
+            expectedMethod: 'GET',
+            expectedPath: '/1/members/my-member-id',
+            expectedHeaders: {},
+            expectedQueryParameters: {
+              'boardBackgrounds': 'none',
+              'boardStars': 'false',
+              'boardsInvited': 'all',
+              'boardsInvitedFields': 'all',
+              'cars': 'none',
+              'customBoardBackground': 'none',
+              'customEmoji': 'none',
+              'customStickers': 'none',
+              'fields':
+                  'id,username,email,fullName,initials,url,status,memberType,confirmed,bio',
+              'organizations': 'none',
+              'organization_fields': 'all',
+              'organization_paid_account': 'false',
+              'organizationsInvited': 'none',
+              'organizationsInvited_fields': 'all',
+              'paid_account': 'false',
+              'savedSearches': 'false',
+              'tokens': 'none',
+            },
+          )
+        ],
+        output: [
+          'id         | my-member-id  ',
+          'username   | my-username   ',
+          'email      | my-email      ',
+          'fullName   | my-full-name  ',
+          'initials   | my-initials   ',
+          'url        | my-url        ',
+          'status     | my-status     ',
+          'memberType | my-member-type',
+          'confirmed  | true          ',
+          'bio        | my-bio        ',
+        ],
+        help: helpOutput,
+        notFoundOutput: [
+          'ERROR: member get - Failure: Resource not found: /1/members/my-member-id'
+        ],
+        serverErrorOutput: [
+          'ERROR: member get - Failure: GET /1/members/my-member-id'
+        ],
+      ));
+  group(
+      'no member-id',
+      () => cliTest(
+          arguments: 'member get'.split(' '),
+          responses: [],
+          expectations: CliExpectations(
+              requests: [],
+              output: ['ERROR: member get - Failure: Member Id not given'],
+              help: helpOutput)));
 }
