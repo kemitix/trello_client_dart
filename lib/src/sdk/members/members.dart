21149293ab446102c1b420305b7a749429bebab5
Paul Campbell <pcampbell@kemitix.net>
2022-06-06 17:32:23 +0100

    Fold member_models.dart into members.dart

--- lib/src/sdk/members/members.dart
+++ lib/src/sdk/members/members.dart
@@ -1,6 +1,36 @@
+import '../trello_models.dart'
+    show
+        AllOrNone,
+        BoardBackgrounds,
+        BoardsInvited,
+        BoardsInvitedFields,
+        NestedActionsQueryParamsActions,
+        NestedBoardsQueryParamsBoards,
+        NestedCardQueryParametersCards,
+        NestedNotificationsQueryParametersNotifications,
+        OrganizationFields,
+        Organizations,
+        OrganizationsInvited,
+        OrganizationsInvitedFields;
+
 export 'member_board_filter.dart';
 export 'member_client.dart';
 export 'member_fields.dart';
 export 'member_id.dart';
-export 'member_models.dart';
 export 'trello_member.dart';
+
+typedef MemberOrganizations = Organizations;
+typedef MemberTokens = AllOrNone;
+typedef MemberCustomStickers = AllOrNone;
+typedef MemberCustomEmoji = AllOrNone;
+typedef MemberCustomBoardBackground = AllOrNone;
+typedef MemberActions = NestedActionsQueryParamsActions;
+typedef MemberBoards = NestedBoardsQueryParamsBoards;
+typedef MemberCards = NestedCardQueryParametersCards;
+typedef MemberBoardBackgrounds = BoardBackgrounds;
+typedef MemberBoardsInvited = BoardsInvited;
+typedef MemberBoardsInvitedFields = BoardsInvitedFields;
+typedef MemberNotifications = NestedNotificationsQueryParametersNotifications;
+typedef MemberOrganizationFields = OrganizationFields;
+typedef MemberOrganizationsInvited = OrganizationsInvited;
+typedef MemberOrganizationsInvitedFields = OrganizationsInvitedFields;
