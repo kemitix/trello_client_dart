8eec8ee97bf0f59089bb646a3a8c40de8798f0c0
Paul Campbell <pcampbell@kemitix.net>
2022-06-06 09:40:03 +0100

    Add tests for ListHead extension

---
+++ test/src/sdk/extensions/list_test.dart
@@ -0,0 +1,21 @@
+import 'package:test/test.dart';
+import 'package:trello_sdk/trello_sdk.dart';
+
+void main() {
+  test('list.head returns first item on list', () {
+    //given
+    var list = <String>['hello', 'world'];
+    //when
+    String? result = list.head;
+    //then
+    expect(result, 'hello');
+  });
+  test('list.head return null on empty list', () {
+    //given
+    var list = <String>[];
+    //when
+    String? result = list.head;
+    //then
+    expect(result, isNull);
+  });
+}
